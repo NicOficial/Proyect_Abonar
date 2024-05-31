7360ff4d377d3d4736df42cecb7f9f871c8fe271
MichatNovato <valentinmichat04@gmail.com>
2024-05-31 17:39:43 -0300

    tabla de la wallet

--- Back-End/abonar.sql
+++ Back-End/abonar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2024 a las 15:38:38
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 31-05-2024 a las 22:31:14
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,17 +34,28 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
-  `phone` int(11) NOT NULL,
   `postal` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id_users`, `name`, `surname`, `email`, `password`, `address`, `phone`, `postal`) VALUES
-(1, 'Nicolas', 'Primo', 'neque.primo@gmail.com', '123', 'Flora 984', 1167010755, 1706),
-(2, 'Valentin', 'Peluzo', 'panchoflencho@gmail.com', '123', 'monseñor de pancho 66', 1167010755, 1706);
+INSERT INTO `users` (`id_users`, `name`, `surname`, `email`, `password`, `address`, `postal`) VALUES
+(1, 'Nicolas', 'Primo', 'neque.primo@gmail.com', '123', 'Flora 984', 1706),
+(2, 'Valentin', 'Peluzo', 'panchoflencho@gmail.com', '123', 'monseñor de pancho 66', 1706);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `wallets`
+--
+
+CREATE TABLE `wallets` (
+  `id_wallet` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `amount` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
@@ -55,7 +66,15 @@ INSERT INTO `users` (`id_users`, `name`, `surname`, `email`, `password`, `addres
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_users`),
-  ADD UNIQUE KEY `id_users_UNIQUE` (`id_users`);
+  ADD UNIQUE KEY `id_users_UNIQUE` (`id_users`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `wallets`
+--
+ALTER TABLE `wallets`
+  ADD PRIMARY KEY (`id_wallet`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -66,6 +85,22 @@ ALTER TABLE `users`
 --
 ALTER TABLE `users`
   MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `wallets`
+--
+ALTER TABLE `wallets`
+  MODIFY `id_wallet` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `wallets`
+--
+ALTER TABLE `wallets`
+  ADD CONSTRAINT `wallets_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_users`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
