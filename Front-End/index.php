494da7e38f83d931b8a7582e2f1a42e75dc0af3f
MichatNovato <valentinmichat04@gmail.com>
2024-07-08 15:45:00 -0300

    seguimos con la pagina inicial

--- Front-End/index.php
+++ Front-End/index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../Css/index.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
+
 <body>
     <header class="header">
         <a href="#" class="logo">Abonar</a>
@@ -28,11 +31,13 @@
             <h3>Hola, somos</h3>
             <h1>Abonar</h1>
             <h3>Y somos una <span>Wallet</span></h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem mollitia error laborum ut hic tenetur reiciendis rerum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem mollitia error laborum ut hic tenetur
+                reiciendis rerum.</p>
             <div class="social-media">
-                <a href="#"><box-icon type='logo' name='instagram'></box-icon></a>
-                <a href="#"><box-icon name='youtube' type='logo' ></box-icon></a>
-                <a href="#"><box-icon type='logo' name='twitter'></box-icon></a>
+                <a href="#"><i class="bx bxl-youtube"></i></a>
+                <a href="#"><i class="bx bxl-twitter"></i></a>
+                <a href="#"><i class="bx bxl-instagram-alt"></i></a>
+
             </div>
             <a href="#" class="btn">Crear Cuenta</a>
             <a href="#" class="btn">Iniciar sesion</a>
@@ -43,5 +48,48 @@
             <img src="../Img/abonar logo nuevo sin fondo.jpg.png" alt="logo">
         </div>
     </section>
+
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="/Img/abonar logo nuevo sin fondo.jpg.png" alt="logo">
+        </div>
+        <div class="about-content">
+            <h2 class="heading">Sobre <span>Nosotros</span></h2>
+            <h3>Billetera Virtual</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aperiam corrupti animi temporibus amet in
+                aliquam molestiae laborum officia commodi alias quidem nemo, voluptate sequi soluta quaerat debitis
+                consequatur, dolorum numquam! Minima ex doloribus quibusdam saepe aliquam, ut odio corporis quas,
+                nesciunt, eum rerum cupiditate. Dolor!</p>
+            <a href="#" class="btn">Leer Mas</a>
+        </div>
+    </section>
+
+    <section class="services" id="services">
+        <h2 class="heading">Our <span>Services</span></h2>
+        <div class="services-container">
+            <div class="services-box">
+                <i class="bx bx-code-alt"></i>
+                <h3>Web Development</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam ex debitis amet magnam, excepturi
+                    tempora ratione ullam quasi dicta laborum.</p>
+                    <a href="#" class="btn">Read More</a>
+            </div>
+            <div class="services-box">
+                <i class="bx bxs-paint"></i>
+                <h3>Graphic Design</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam ex debitis amet magnam, excepturi
+                    tempora ratione ullam quasi dicta laborum.</p>
+                    <a href="#" class="btn">Read More</a>
+                    <div class="services-box">
+                        <i class="bx bx-bar-chart-alt"></i>
+                        <h3>Digital Marketing</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam ex debitis amet magnam, excepturi
+                            tempora ratione ullam quasi dicta laborum.</p>
+                            <a href="#" class="btn">Read More</a>
+
+            
+        </div>
+    </section>
 </body>
+
 </html>
