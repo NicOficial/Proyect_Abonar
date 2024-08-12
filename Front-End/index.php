8604004788a785d48f70fcd3fb5cb3efce349513
MichatNovato <valentinmichat04@gmail.com>
2024-08-12 14:50:26 -0300

    seguimos

--- Front-End/index.php
+++ Front-End/index.php
@@ -144,6 +144,33 @@
             </div>
         </div>
     </section>
+
+    <section class="contact" id="contact">
+        <h2 class="heading">Contact <span>Me!</span></h2>
+
+        <form action="#">
+            <div class="input-box">
+                <input type="text" placeholder="Full Name">
+                <input type="text" placeholder="Email Address">
+
+            </div>
+            <div class="input-box">
+                <input type="number" placeholder="Mobile Number">
+                <input type="text" placeholder="Email Subject">
+
+            </div>
+            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+            <input type="submit" value="Send Message" class="btn">
+        </form>
+    </section>
+
+    <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2024 by Abonar | Todos los derechos reservados.</p>
+        </div>
+
+        <div class="footer-iconTop"></div>
+    </footer>
 </body>
 
 </html>
